76ec05442c998d6dab76bdce31b98e8dd147119a
Valeriereds <valeriereds@gmail.com>
2023-08-08 20:27:26 -0500

    copied repo files, added model info, started routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  try{
+    const catData = await Category.findAll();
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // find all categories
+  // be sure to include its associated Products
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const catData = await Category.findByPk
+    (req.params.id, {
+      include: [{ model: Product}]
+    });
+    if (!catData) {
+      res.status(400).json({ message: 'No Category found with this id!'});
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// find one category by its `id` value
+// be sure to include its associated Products
+router.post('/', async (req, res) => {
+  try {
+    const catData = await Category.create(req.body);
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  // create a new category
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const catData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catData[0]) {
+      res.status(404).json({ message: 'No category with this id!'});
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // update a category by its `id` value
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const catData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catData) {
+      res.status(404).json({ message: 'No category with this id!'});
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // delete a category by its `id` value
+});
+
+module.exports = router;
